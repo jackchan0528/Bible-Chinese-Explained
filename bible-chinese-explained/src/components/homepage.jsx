a9a105f5de8f91acb81d11552240dc31d57933cb
Jack Chan <46509894+jackchan0528@users.noreply.github.com>
2022-12-27 22:38:11 +0800

    Update homepage.jsx

--- bible-chinese-explained/src/components/homepage.jsx
+++ bible-chinese-explained/src/components/homepage.jsx
@@ -44,7 +44,7 @@ const HomePage = (props) => {
             onChange={(e) => props.onSelectedChapter(e.target.value)}
             className=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 px-2 py-2"
           >
-            <option value="DEFAULT" disabled>
+            <option value="" disabled={true}>
               請選擇
             </option>
             <GetNumberOfChaptersList value={props.selectedBook} />
