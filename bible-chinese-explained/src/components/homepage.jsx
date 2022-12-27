f9b3b99743e132b7be1c3dbc117f7f1b0b71492f
Jack Chan <46509894+jackchan0528@users.noreply.github.com>
2022-12-27 20:38:48 +0800

    select new book will auto change chap to 1

--- bible-chinese-explained/src/components/homepage.jsx
+++ bible-chinese-explained/src/components/homepage.jsx
@@ -16,8 +16,11 @@ const HomePage = (props) => {
           <select
             id="books"
             //   ref={(input) => (this.menu = input)}
-            value={props.value}
-            onChange={(e) => props.onSelectedBook(e.target.value)}
+            value={props.selectedBook}
+            onChange={(e) => {
+              props.onSelectedBook(e.target.value);
+              props.onSelectedChapter(1);
+            }}
             className=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 px-2 py-2"
           >
             <option value="DEFAULT" disabled>
@@ -36,7 +39,7 @@ const HomePage = (props) => {
           <select
             id="chapters"
             //   ref={(input) => (this.menu = input)}
-            value={props.value}
+            value={props.selectedChapter}
             onChange={(e) => props.onSelectedChapter(e.target.value)}
             className=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 px-2 py-2"
           >
